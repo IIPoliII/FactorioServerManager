a29c8b3b3ff4c8266c9de905e4137668581eb06d
Poli <admin@poli.fun>
2020-02-11 14:03:46 +0100

    Update service.sh

--- service.sh
+++ service.sh
@@ -1,6 +1,6 @@
 # !/bin/bash
-# Installs FactoCord as a systemd service
-# Script by Allen Lydiard (FM1337)
+# Installs JAPC BOT as a systemd service
+# Script by Allen Lydiard (FM1337), Rewroted by Poli
 
 
 # First let's check if the user is running this script as root.
@@ -16,7 +16,7 @@ if [ $# -eq 0 ]; then
 	echo "You didn't supply any arguments!"
 	echo "You must supply the following arguments"
 	echo "1. The user to run the service as: e.g factorio"
-	echo "2. The working directory: e.g /home/factorio/factocord"
+	echo "2. The working directory: e.g /home/factorio/japcbot"
 	exit 1
 fi
 
@@ -30,29 +30,27 @@ fi
 # We also need to check if the working directory exists.
 if [ ! -d "$2" ]; then
 	echo "Sorry the working directory you supplied does not exist."
-	echo "Example of working directory: /home/factorio/factocord/"
+	echo "Example of working directory: /home/factorio/japcbot/"
 	exit 1
 fi
 
 # Moving on we need to check if the start.sh script exists in the working directory.
 if [ ! -f "$2/start.sh" ]; then
 	echo "Sorry the start.sh script isn't in the working directory."
-	echo "You can fetch a copy of it from here https://github.com/FactoKit/FactoCord/tree/master/linux"
 	exit 1
 fi
 
 # After we verified start.sh exist, we need to check if the FactoCord binary exists.
-if [ ! -f "$2/FactoCord" ]; then
-	echo "Sorry the FactoCord binary is missing from your working directory"
-	echo "You can get a copy of it from here https://github.com/FactoKit/FactoCord/releases"
+if [ ! -f "$2/JAPCBOT" ]; then
+	echo "Sorry the JAPC BOT binary is missing from your working directory"
 	exit 1
 fi
 
-# Last but not least, we check if the factocord service is already installed and ask if it should be
+# Last but not least, we check if the JAPC BOT service is already installed and ask if it should be
 # reinstalled.
 if [ -f "/etc/systemd/system/factocord.service" ]; then
 	while true; do
-		echo "It looks like Factocord is already installed a service"
+		echo "It looks like JAPC BOT is already installed a service"
 		read -p "Would you like to reinstall it? " yn
 		case $yn in
 			[Yy]* ) systemctl disable factocord.service; break;;
@@ -64,14 +62,14 @@ fi
 
 
 ServiceVar="[Unit]
-Description=FactoCord Bot
+Description=JAPC Bot
 Wants=network-online.target
 After=network.target network-online.target
 
 [Service]
 User=$1
 WorkingDirectory=$2
-PIDFile=$2/factocord.pid
+PIDFile=$2/japcbot.pid
 ExecStart=/bin/bash start.sh
 Restart=on-failure
 StartLimitInterval=600
@@ -79,8 +77,8 @@ StartLimitInterval=600
 [Install]
 WantedBy=multi-user.target"
 
-echo "$ServiceVar" > /etc/systemd/system/factocord.service
+echo "$ServiceVar" > /etc/systemd/system/japcbot.service
 
 systemctl daemon-reload
-systemctl enable factocord.service
-systemctl start factocord.service
+systemctl enable japcbot.service
+systemctl start japcbot.service
